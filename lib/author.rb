03427d81fb51ea9f8d03805e503837f59094c32b
Joseph Burks <joe_burks_3@yahoo.com>
2019-11-03 16:15:20 -0600

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,39 @@
+class Author
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all.push(self)
+    end
+
+    def self.all
+        @@all
+    end
+
+    def name
+        @name
+    end
+
+    def posts
+        Post.all.select do |post|
+            post.author == self
+        end
+    end
+
+    def add_post(post)
+        post.author = self
+    end
+
+    def self.post_count
+        Post.all.length
+    end
+
+    def add_post_by_title(post_title)
+        post = Post.new(post_title)
+        post.title = post_title
+        post.author = self
+    end
+
+
+end
